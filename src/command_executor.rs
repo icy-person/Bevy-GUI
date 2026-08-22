d01ca39bae7cc5496801ec419a95e2a6f1d39f9a
icy-person <kar.fars.1388@gmail.com>
2026-08-22 22:31:17 +0330

    add real command executor

---
+++ src/command_executor.rs
@@ -0,0 +1,41 @@
+use bevy::prelude::*;
+
+use crate::{
+    assets::AssetDatabase,
+    command::{EditorCommandBus, EditorCommandId},
+    project::{save_project, EditorMode, ProjectState},
+};
+
+#[derive(Resource, Default, Debug)]
+pub struct CommandExecutionState {
+    pub executed: u64,
+    pub last: Option<EditorCommandId>,
+    pub last_error: Option<String>,
+}
+
+pub fn execute_editor_commands(
+    mut bus: ResMut<EditorCommandBus>,
+    mut project: ResMut<ProjectState>,
+    mut assets: ResMut<AssetDatabase>,
+    mut state: ResMut<CommandExecutionState>,
+) {
+    for id in bus.drain() {
+        state.executed = state.executed.saturating_add(1);
+        state.last = Some(id);
+        state.last_error = None;
+        match id.0 {
+            "project.save" => {
+                if let Err(error) = save_project(&project.root, &project) {
+                    state.last_error = Some(error.to_string());
+                } else {
+                    project.dirty = false;
+                }
+            }
+            "project.play" => project.mode = EditorMode::Play,
+            "project.pause" => project.mode = EditorMode::Paused,
+            "project.stop" => project.mode = EditorMode::Edit,
+            "assets.refresh" => assets.refresh_requested = true,
+            _ => {}
+        }
+    }
+}
