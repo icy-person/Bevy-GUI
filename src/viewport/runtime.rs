27e08e1056ca29a716dcda5a8012dac9bcdc5733
icy-person <kar.fars.1388@gmail.com>
2026-08-22 23:01:52 +0330

    fix runtime scene snapshot tuple shape

--- src/viewport/runtime.rs
+++ src/viewport/runtime.rs
@@ -1,23 +1,33 @@
 use bevy::prelude::*;
 use std::collections::BTreeMap;
 
-use crate::{project::{EditorMode, ProjectState}, runtime::PlaySession, scene::SceneDocument};
+use crate::{
+    project::{EditorMode, ProjectState},
+    runtime::PlaySession,
+    scene::SceneDocument,
+    scene_model::EditorParent,
+};
 
 use super::components::EditorEntity;
 
 pub fn apply_runtime_mode(
     project: Res<ProjectState>,
     mut session: ResMut<PlaySession>,
-    mut query: Query<(Entity, Option<&Name>, &mut Transform), With<EditorEntity>>,
+    mut query: Query<
+        (Entity, Option<&Name>, &mut Transform, Option<&EditorParent>),
+        With<EditorEntity>,
+    >,
 ) {
     match project.mode {
         EditorMode::Play if session.snapshot.is_none() => {
             let snapshot = SceneDocument::from_entities(query.iter_mut().map(
-                |(_, name, transform)| {
+                |(entity, name, transform, parent)| {
                     (
+                        entity,
                         name.map(|value| value.as_str().to_owned())
                             .unwrap_or_else(|| "Entity".into()),
                         *transform,
+                        parent.and_then(|value| value.0),
                     )
                 },
             ));
@@ -32,7 +42,7 @@ pub fn apply_runtime_mode(
                     .into_iter()
                     .map(|entity| (entity.name.clone(), entity))
                     .collect();
-                for (_, name, mut transform) in &mut query {
+                for (_, name, mut transform, _) in &mut query {
                     if let Some(saved) = name.and_then(|value| saved.get(value.as_str())) {
                         transform.translation = Vec3::from_array(saved.translation);
                         transform.rotation = Quat::from_xyzw(
